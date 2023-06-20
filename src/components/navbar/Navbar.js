b7836dbd01cca9060d1197b4a5311a9f900bba35
Sidharth <sidharth.a.satheesh@gmail.com>
2023-06-21 00:22:20 +0530

    responsiveness

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.scss'
-import { NavLink } from 'react-router-dom'
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  // return (
+  //   <nav className='navbar-main'>
+  //       <NavLink className='nav-item' to='/'>Home</NavLink>
+  //       <NavLink className='nav-item' to='/about'>About Us</NavLink>
+  //       <NavLink className='nav-item' to='/privacy'>Privacy Policy</NavLink>
+  //       <NavLink className='nav-item' to='/terms'>Terms & conditions</NavLink>
+  //       <NavLink className='nav-item' to='/contact'>Contact Us</NavLink>
+  //   </nav>
+  const [viewNav, setViewNav] = useState(false);
+  // const navRef = useRef();
+  const showNavbar = () => {
+    setViewNav(!viewNav);
+    // navRef.current.classList.toggle("responsive-nav");
+  };
+
   return (
-    <nav className='navbar-main'>
-        <NavLink className='nav-item' to='/'>Home</NavLink>
-        <NavLink className='nav-item' to='/about'>About Us</NavLink>
-        <NavLink className='nav-item' to='/privacy'>Privacy Policy</NavLink>
-        <NavLink className='nav-item' to='/terms'>Terms & conditions</NavLink>
-        <NavLink className='nav-item' to='/contact'>Contact Us</NavLink>
-    </nav>
+    <header className='header-sub'>
+      {/* <Link to="/">
+        <img className="nav-logo" src={logo} alt="" />
+      </Link> */}
+      <div className="nav">
+        <nav className={viewNav ? "responsive-nav nav-sub" : "nav-sub-link"}>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+          {/* <Link className="nav-link" to="/about">
+            About Us
+          </Link> */}
+          <Link className="nav-link" to="/privacy">
+          Privacy Policy
+          </Link>
+          <Link className="nav-link" to="/terms">
+          Terms & conditions
+          </Link>
+          <Link className="nav-link" to="/contact">
+          Contact Us
+          </Link>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
+      </div>
+    </header>
+
   )
 }
 
